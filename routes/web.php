f6ee8250bcc3e20a0bdfd0c4e1cf266f65213fc9
zainibrahem <zainibrahem46@gmail.com>
2023-06-09 15:58:46 -0500

    authentication added

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('admin.dashboard');
-});
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
