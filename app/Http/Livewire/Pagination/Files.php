b77de10a4c443d443202cdf3cce6bcaa9d6c2984
mahmoud.hamed <mahmoud.hamed@itfaq.global>
2023-06-12 00:06:03 +0400

    remove warnings

--- app/Http/Livewire/Pagination/Files.php
+++ app/Http/Livewire/Pagination/Files.php
@@ -5,8 +5,14 @@ namespace App\Http\Livewire\Pagination;
 use App\Models\File;
 use App\Models\Stage;
 use App\Models\Subject;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Livewire\WithPagination;
@@ -16,13 +22,13 @@ class Files extends Component
     use WithPagination;
     use WithFileUploads;
 
-    protected $paginationTheme = 'bootstrap';
-    public $searchTerm;
-    public $limit = 10;
-    public $isCreate = false;
-    public $subjects;
+    protected string $paginationTheme = 'bootstrap';
+    public string $searchTerm;
+    public int $limit = 10;
+    public bool $isCreate = false;
+    public Collection $subjects;
 
-    public $name, $file, $path, $subject;
+    public  $name, $file, $path, $subject;
 
 
     public function showModel()
@@ -31,19 +37,25 @@ class Files extends Component
     }
 
 
-    protected $rules = [
+    protected array $rules = [
         'name' => 'required|min:4',
         'subject' => 'required|exists:subjects,id',
         'file' => 'required|file|max:1024',
     ];
 
+    /**
+     * @throws ValidationException
+     */
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
     }
 
 
-    public function submit()
+    /**
+     * @return RedirectResponse
+     */
+    public function submit(): RedirectResponse
     {
 
         $this->validate();
@@ -53,7 +65,7 @@ class Files extends Component
 
         $data = Str::replace('public', 'storage', $data);
 
-        File::create([
+        File::query()->create([
             'name' => $this->name,
             'subject_id' => $this->subject,
             'type' => $this->file->getMimeType(),
@@ -63,7 +75,10 @@ class Files extends Component
     }
 
 
-    public function render()
+    /**
+     * @return Application|Factory|View|\Illuminate\Foundation\Application
+     */
+    public function render(): \Illuminate\Foundation\Application|View|Factory|Application
     {
         //todo filter subject that belongs to the Auth user
         //todo add the subjects that has been created by admin
@@ -72,10 +87,11 @@ class Files extends Component
         $searchTerm = '%' . $this->searchTerm . '%';
         $limit = $this->limit;
 
-        $files = File::query()->
-        where("name", 'like', $searchTerm)->
-        where('created_by', Auth::id())->
-        paginate($limit);
-        return view('livewire.pagination.files', ['files' => $files]);
+        $files = File::query()
+            ->where("name", 'like', $searchTerm)
+            ->where('created_by', Auth::id())
+            ->paginate($limit);
+        return view('livewire.pagination.files',
+            ['files' => $files]);
     }
 }
